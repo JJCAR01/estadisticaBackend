4b0e8533ace05ce1c938a7b9a09edeadd4e5a4ab
JUANCARDONA <juanjo-cardona@hotmail.com>
2022-05-31 14:57:03 -0500

    Inicio de Proyecto

---
+++ estadistica/src/main/java/com/juan/estadistica/EstadisticaApp.java
@@ -0,0 +1,13 @@
+package com.juan.estadistica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstadisticaApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstadisticaApp.class, args);
+	}
+
+}
---
+++ estadistica/src/test/java/com/juan/estadistica/EstadisticaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juan.estadistica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstadisticaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
